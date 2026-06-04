79c9b937e2ce1f7053c9d6c9a7630658427a44af
prgram769 <prgram02@gmail.com>
2026-06-04 19:51:46 +0200

    reading image

--- ImgToAscii.java
+++ ImgToAscii.java
@@ -1,14 +1,27 @@
 import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class ImgToAscii {
 
   public static void convertImageToAscii(File imgToConvert) {
-    
+    try {
+      BufferedImage originalImage = ImageIO.read(imgToConvert);
+
+      int test = originalImage.getHeight();
+      int test2 = originalImage.getWidth();
+      
+      System.out.println(originalImage);
+      System.out.println(test);
+      System.out.println(test2);
+    } catch (Exception e) {
+      System.err.println(e);
+    }
   }
 
   public static void main(String[] args) {
     File myImage = new File(args[0]);
 
-
+    convertImageToAscii(myImage);
   }
 }
