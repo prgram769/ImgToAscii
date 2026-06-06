3213871158bb30e84c9614c6a7f69b0f41136292
prgram769 <prgram02@gmail.com>
2026-06-06 16:48:49 +0200

    program finished

--- ImgToAscii.java
+++ ImgToAscii.java
@@ -1,27 +1,81 @@
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class ImgToAscii {
+  public static BufferedImage resizeImage(File imgToResize, int width) {
+    BufferedImage smallImg = null;
 
-  public static void convertImageToAscii(File imgToConvert) {
     try {
-      BufferedImage originalImage = ImageIO.read(imgToConvert);
+      BufferedImage originalImage = ImageIO.read(imgToResize);
 
-      int test = originalImage.getHeight();
-      int test2 = originalImage.getWidth();
-      
-      System.out.println(originalImage);
-      System.out.println(test);
-      System.out.println(test2);
+      int newWidth = width;
+      int newHight = (int) (originalImage.getHeight() * newWidth / (double) originalImage.getWidth() / 2);
+
+      Image imgResized = originalImage.getScaledInstance(newWidth, newHight, Image.SCALE_SMOOTH);
+
+      smallImg = new BufferedImage(newWidth, newHight, BufferedImage.TYPE_INT_RGB);
+
+      Graphics2D g2d = smallImg.createGraphics();
+
+      g2d.drawImage(imgResized, 0, 0, null);
+
+      g2d.dispose();
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+
+    return smallImg;
+  }
+
+  public static void convertImageToAscii(BufferedImage imgToConvert) {
+    try {
+      int height = imgToConvert.getHeight();
+      int width = imgToConvert.getWidth();
+
+      String symbols = "@&#%$*+=-;:,.'` ";
+
+      for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+          int rgb = imgToConvert.getRGB(x, y);
+          int r = (rgb >> 16) & 0xFF;
+          int g = (rgb >> 8) & 0xFF;
+          int b = rgb & 0xFF;
+
+          double bright = 0.299 * r + 0.587 * g + 0.114 * b;
+
+          int indexNumber = (int) (bright * (symbols.length() - 1) / 255);
+
+          System.out.print(symbols.charAt(indexNumber));
+        }
+        System.out.println();
+      }
+
+      System.out.printf("\n\nMade with ❤️ by rugby01");
     } catch (Exception e) {
       System.err.println(e);
     }
   }
 
   public static void main(String[] args) {
-    File myImage = new File(args[0]);
+    File myImage = null;
 
-    convertImageToAscii(myImage);
+    int newWidth = 0;
+
+    try {
+      myImage = new File(args[0]);
+
+      newWidth = Integer.parseInt(args[1]);
+    } catch (Exception e) {
+      System.err.printf("To use this program you need to specify in the terminal the file's path and the new width to resize it.\n");
+
+      return;
+    }
+
+    BufferedImage resizedImg = resizeImage(myImage, newWidth);
+
+    convertImageToAscii(resizedImg);
   }
 }
