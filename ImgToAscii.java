106b2bfca0ec7d291dd1feb167abdcff94921979
prgram769 <prgram02@gmail.com>
2026-06-06 17:26:38 +0200

    program finished with a new function to export image

--- ImgToAscii.java
+++ ImgToAscii.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -30,12 +32,54 @@ public class ImgToAscii {
     return smallImg;
   }
 
-  public static void convertImageToAscii(BufferedImage imgToConvert) {
+  public static void convertImageToAscii(BufferedImage imgToConvert, String imgFile) {
     try {
       int height = imgToConvert.getHeight();
       int width = imgToConvert.getWidth();
+      
+      String symbols = "@&#%$*+=-;:,. ";
 
-      String symbols = "@&#%$*+=-;:,.'` ";
+      if (imgFile.equals("yes")) {
+        int charWidth = 7;
+        int charHeight = 14;
+        int imgWidth = charWidth * width;
+        int imgHeight = charHeight * height;
+
+        BufferedImage asciiImg = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
+
+        Font font = new Font("Courier New", Font.PLAIN, 12);
+
+        Graphics2D g2d = asciiImg.createGraphics();
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, asciiImg.getWidth(), asciiImg.getHeight());
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(font);
+
+        for (int y = 0; y < height; y++) {
+          for (int x = 0; x < width; x++) {
+            int rgb = imgToConvert.getRGB(x, y);
+            int r = (rgb >> 16) & 0xFF;
+            int g = (rgb >> 8) & 0xFF;
+            int b = rgb & 0xFF;
+
+            double bright = 0.299 * r + 0.587 * g + 0.114 * b;
+
+            int indexNumber = (int) (bright * (symbols.length() - 1) / 255);
+
+            g2d.drawString(String.valueOf(symbols.charAt(indexNumber)), x * charWidth, (y + 1) * charHeight);
+          }
+        }
+
+        g2d.dispose();
+
+        ImageIO.write(asciiImg, "png", new File("asciiArt.png"));
+
+        System.out.println("Image saved as asciiArt.png");
+        System.out.printf("\n\nMade with ❤️ by rugby01");
+
+        return;
+      }
 
       for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
@@ -64,18 +108,27 @@ public class ImgToAscii {
 
     int newWidth = 0;
 
+    String imgFile = "";
+    String errorMessage = "To use this program you need to specify in the terminal the file's path and the new width to resize it. If you want to convert the output in one new img you need to specify in the 3rd field of the terminal yes, if not no\n";
+
     try {
       myImage = new File(args[0]);
 
       newWidth = Integer.parseInt(args[1]);
+
+      imgFile = args[2].toLowerCase();
+
+      if (!imgFile.equals("yes") && !imgFile.equals("no")) {
+        throw new Exception(errorMessage);
+      }
     } catch (Exception e) {
-      System.err.printf("To use this program you need to specify in the terminal the file's path and the new width to resize it.\n");
+      System.err.printf(errorMessage);
 
       return;
     }
 
     BufferedImage resizedImg = resizeImage(myImage, newWidth);
 
-    convertImageToAscii(resizedImg);
+    convertImageToAscii(resizedImg, imgFile);
   }
 }
